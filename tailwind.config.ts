c8535ab52b892cb829921cf2cd2e9814bacc66b9
brepositorium <rcryptob@protonmail.ch>
2024-08-09 22:18:30 +0300

    add custom border radius to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,13 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    borderRadius: {
+      sm: "2px",
+      md: "6px",
+      lg: "8px",
+      xl: "12px",
+      large: "28px",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
