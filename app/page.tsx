57e6d62c8edb877c791054e885de7b05f0faa42f
brepositorium <rcryptob@protonmail.ch>
2024-08-11 10:54:41 +0300

    redesign landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 import React from "react";
-import Link from "next/link";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import HowItWorks from "@/components/HowItWorks";
+import LaunchButton from "@/components/LaunchButton";
 
 const Home = () => {
   return (
@@ -11,7 +12,7 @@ const Home = () => {
         initial={{ y: -50, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5 }}
-        className="w-full text-center mb-12"
+        className="w-full text-center mb-20 mt-4"
       >
         <Image
           src="/logo.png"
@@ -23,31 +24,23 @@ const Home = () => {
       </motion.header>
 
       <main className="w-full max-w-6xl mx-auto">
-        <section className="flex flex-col md:flex-row items-center justify-between mb-16">
+        <section className="flex flex-col md:flex-row items-center justify-between mb-20">
           <motion.div
             initial={{ x: -50, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
             transition={{ duration: 0.5, delay: 0.2 }}
             className="md:w-1/2 mb-8 md:mb-0"
           >
-            <h1 className="text-4xl font-bold mb-4">
-              Authenticate Your Digital Collectibles
+            <h1 className="text-4xl text-[#1b577f] tracking-wide font-bold mb-10 font-poppins">
+              <span className="font-pacifico text-6xl text-gray-900">Sign</span>{" "}
+              any NFT
             </h1>
-            <p className="text-xl mb-6">
+            <p className="text-xl mb-10 font-poppins pr-24 text-gray-500">
               StarSeal allows celebrities, influencers, and creators to
-              digitally sign NFTs, adding a layer of authenticity and personal
-              connection to digital collectibles.
+              digitally autograph NFTs, adding a layer of authenticity and
+              personal connection to digital collectibles.
             </p>
-            <Link href="/app" passHref>
-              <motion.button
-                className="relative bg-primary text-white font-bold py-2 px-4 rounded hover:bg-secondary transition duration-300 overflow-hidden group"
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.95 }}
-              >
-                <span className="relative z-10">Launch App</span>
-                <span className="absolute bottom-0 left-0 w-full h-1 bg-white transform -translate-x-full group-hover:translate-x-0 transition-transform duration-300"></span>
-              </motion.button>
-            </Link>
+            <LaunchButton href="/app">Launch App</LaunchButton>
           </motion.div>
           <motion.div
             initial={{ x: 50, opacity: 0 }}
@@ -56,57 +49,12 @@ const Home = () => {
             className="md:w-1/2"
           >
             <div className="aspect-w-16 aspect-h-9">
-              <iframe
-                src="https://www.youtube.com/embed/your-video-id"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                className="w-full h-full rounded-lg shadow-lg"
-              ></iframe>
+              <img src="/signing.gif" className="rounded-md" />
             </div>
           </motion.div>
         </section>
 
-        <section className="text-center mb-16">
-          <motion.h2
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.5, delay: 0.6 }}
-            className="text-3xl font-bold mb-8"
-          >
-            How It Works
-          </motion.h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-            {[
-              {
-                title: "1. Connect",
-                description:
-                  "Connect your wallet to the StarSeal app using Web3 technology.",
-              },
-              {
-                title: "2. Sign",
-                description:
-                  "Enter the NFT details and your personal message to create a unique attestation.",
-              },
-              {
-                title: "3. Verify",
-                description:
-                  "Anyone can easily verify the authenticity of the signed NFT using our app.",
-              },
-            ].map((step, index) => (
-              <motion.div
-                key={step.title}
-                initial={{ y: 50, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{ duration: 0.5, delay: 0.8 + index * 0.2 }}
-                className="bg-white p-6 rounded-lg shadow-md"
-              >
-                <h3 className="text-xl font-semibold mb-2">{step.title}</h3>
-                <p>{step.description}</p>
-              </motion.div>
-            ))}
-          </div>
-        </section>
+        <HowItWorks />
       </main>
 
       <motion.footer
@@ -115,7 +63,9 @@ const Home = () => {
         transition={{ duration: 0.5, delay: 1.4 }}
         className="w-full text-center py-4"
       >
-        <p>&copy; 2024 StarSeal. All rights reserved.</p>
+        <p className="text-gray-400">
+          &copy; 2024 StarSeal. All rights reserved.
+        </p>
       </motion.footer>
     </div>
   );
